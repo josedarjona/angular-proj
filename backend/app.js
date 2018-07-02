a0b18570959bd808581dbd3fcfe599bc0d8180e9
jose arjona <josedarjona@gmail.com>
2018-07-01 22:27:27 -0400

    fetch comments, id into _id

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+
+const Post = require('./models/post')
 
 const app = express();
 
+mongoose.connect("mongodb+srv://jose:nap0mFwCrPAimXhj@cluster0-uvowy.mongodb.net/node-angular?retryWrites=true")
+  .then(() => {
+      console.log('Connected to DB');
+
+  })
+  .catch(() => {
+      console.log('Connection Failed');
+  })
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }));
+
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
@@ -22,8 +36,12 @@ app.use((req, res, next) => {
 
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  // console.log(post);
+  post.save();
   res.status(201).json({
     message: "post added succesful"
   });
@@ -31,24 +49,29 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-     id: 'aawfaefa23e33',
-     title: 'first server side post',
-     content: 'this is coming from the server'
+  //dummy data for testing
+  // const posts = [
+  //   {
+  //    id: 'aawfaefa23e33',
+  //    title: 'first server side post',
+  //    content: 'this is coming from the server'
 
-    },
-    {
-      id: 'rgsrgx234',
-      title: 'second server side post',
-      content: 'this is coming from the server'
+  //   },
+  //   {
+  //     id: 'rgsrgx234',
+  //     title: 'second server side post',
+  //     content: 'this is coming from the server'
 
-     },
-  ];
+  //    },
+  // ];
 
-  res.status(200).json({
-    message: 'posts fetched succesfully',
-    posts: posts,
+  Post.find()
+  .then(documents => {
+    console.log(documents);
+    res.status(200).json({
+      message: 'posts fetched succesfully',
+      posts: documents,
+    });
   });
 
 });
