b542dd4458b53f595d21ba442de967e570b8801a
jose arjona <josedarjona@gmail.com>
2018-07-03 00:56:36 -0400

    editing and loading previous information even on refresh if needed

--- backend/app.js
+++ backend/app.js
@@ -103,6 +103,23 @@ app.get('/api/posts', (req, res, next) => {
 
 });
 
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+
+    if (post) {
+
+      res.status(200).json(post);
+
+    } else {
+
+      res.status(404).json({message: "Post Not Found"});
+
+    }
+
+
+  })
+})
+
 
 app.delete("/api/posts/:id", (req, res, next) => {
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -42,7 +42,8 @@ export class PostsService {
 
   getPost(id: string) {
 
-    return {...this.posts.find(p => p.id === id)};
+    // return {...this.posts.find(p => p.id === id)};
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
 
 
   }
@@ -70,7 +71,15 @@ export class PostsService {
     const post: Post = { id: id, title: title, content: content };
 
     this.http.put('http://localhost:3000/api/posts/' + id, post)
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+
+      });
 
   }
 
