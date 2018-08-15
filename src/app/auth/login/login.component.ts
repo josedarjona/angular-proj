017f2b2907d6873fe4bc220d2063d0abb0d6febe
jose arjona <josedarjona@gmail.com>
2018-08-14 20:00:07 -0400

    login edits

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,3 +1,26 @@
+// import { Component } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+
+
+
+// @Component({
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css'],
+
+// })
+
+
+// export class LoginComponent {
+//   isLoading = false;
+
+//   onLogin(form: NgForm) {
+
+//     console.log(form.value);
+
+//   }
+
+// }
+
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -20,3 +43,4 @@ export class LoginComponent {
   }
 
 }
+
